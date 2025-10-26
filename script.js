a4e3cb00b6e62dc2228354fdc68961ebb946fb2c
Prakashsingh22 <109896446+Prakashsingh22@users.noreply.github.com>
2025-10-26 15:20:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-function stringChop(str, size) {
+function stringChop(str, chunkLength) {
   // your code here
+	
+  // Handle null or invalid input
+  if (str == null || chunkLength <= 0) return [];
+
+  const chunks = [];
+  for (let i = 0; i < str.length; i += chunkLength) {
+    chunks.push(str.slice(i, i + chunkLength));
+  }
+
+  return chunks;
 }
 
+
 // Do not change the code below
-const str = prompt("Enter String.");
+const str = prompt("Enter String."); 
 const size = prompt("Enter Chunk Size.");
 alert(stringChop(str, size));
